4864d6acbb2377028a36ce22fc88073a5902c6d9
Francesco Terenzani <f.terenzani@gmail.com>
2020-10-08 15:46:44 +0200

    Format lines

--- PackageInstallerPlugin.php
+++ PackageInstallerPlugin.php
@@ -10,7 +10,7 @@ class PackageInstallerPlugin implements PluginInterface
 {
 	public function activate(Composer $composer, IOInterface $io)
 	{
-        $installer = new PackageInstaller($io, $composer);
-        $composer->getInstallationManager()->addInstaller($installer);
+		$installer = new PackageInstaller($io, $composer);
+		$composer->getInstallationManager()->addInstaller($installer);
 	}
 }
